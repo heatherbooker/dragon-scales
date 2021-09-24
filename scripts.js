43b1b5d95efcab520fec139105d2697aa8a3fbf6
hboo <heather.booker@hotmail.com>
2021-09-24 18:01:19 -0400

    introduce a little randomness into the tempo

--- scripts.js
+++ scripts.js
@@ -110,11 +110,30 @@ window.addEventListener('load', function() {
   return scaletype;
   };
 
+  function getRandom(min, max) {
+    let u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    
+    num = num / 10.0 + 0.5; // Translate to 0 -> 1
+    if (num > 1 || num < 0) {
+      num = getRandom(min, max); // resample between 0 and 1 if out of range
+    }
+    
+    else {
+      num *= max - min; // Stretch to fill range
+      num += min; // offset to min
+    }
+    return num;
+  }
+
   function selectSpeed(difficultyLevel) {
     const min = 44;
     const max = 250;
-    const speed = Math.floor(difficultyLevel * 80);
-    return Math.min(250, Math.max(44, speed));
+    let speed = difficultyLevel * 80;
+    speed = getRandom(speed - 10, speed + 10);
+    return Math.floor(Math.min(250, Math.max(44, speed)));
   }
 
   function selectScale(level) {
