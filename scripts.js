0e9aa646040e1bfb7817ef30c76d8dda61a28df3
hboo <heather.booker@hotmail.com>
2021-09-29 23:28:24 -0400

    prepare key signature order

--- scripts.js
+++ scripts.js
@@ -205,25 +205,34 @@ window.addEventListener('load', function() {
 
 
   function how_to_draw_sharps(staff) {
-    const sharp = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    sharp.setAttribute('d', FLAT_SVG_PATH);
-    const sharp_x = (440);
-    const sharp_y = (100);
-    sharp.setAttribute('transform', `translate(${sharp_x} , ${sharp_y})`);
-    staff.appendChild(sharp);
+    sharp_sig_positions.forEach((pos, idx) => {
+
+      const sharp = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      sharp.setAttribute('d', SHARP_SVG_PATH);
+      const sharp_x = (450 + 20*idx);
+      const sharp_y = pos;
+      sharp.setAttribute('transform', `translate(${sharp_x} , ${sharp_y})`);
+      staff.appendChild(sharp);
+    });
   }
 
   function and_flats(staff){
-    const flat = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    flat.setAttribute('d', FLAT_SVG_PATH);
-    flat.setAttribute('class', 'flatty');
-    const flat_x = (340);
-    const flat_y = (93);
-    flat.setAttribute('transform', `translate(${flat_x} , ${flat_y})`);
-    staff.appendChild(flat);
+    flat_signature_positions.forEach((pos, idx) => {
+      const flat = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      flat.setAttribute('d', FLAT_SVG_PATH);
+      flat.setAttribute('class', 'flatty');
+      const flat_x = 340 + 20*idx;
+      const flat_y = pos;
+      flat.setAttribute('transform', `translate(${flat_x} , ${flat_y})`);
+      staff.appendChild(flat);
+    });
   }
 });
 
-const SHARP_SVG_PATH = "M 3.2167 24.5 h 2.9201 l 1.6939 -6.8845 h 6.113 L 12.2695 24.5 h 2.9201 l 1.6939 -6.8845 h 5.996 v -2.4081 h -5.412 l 1.46 -5.8662 h 3.952 v -2.4081 H 19.5115 L 21.2054 0 H 18.2655 l -1.6939 6.9331 H 10.4395 L 12.1334 0 h -2.9207 l -1.6932 6.9331 H 1.6206 v 2.4081 h 5.3149 l -1.4409 5.8662 h -3.874 v 2.4081 h 3.29 L 3.2167 24.5 z M 9.8555 9.3412 h 6.1321 l -1.46 5.8662 H 8.4146 L 9.8555 9.3412 z";
+const flat_signature_positions = [ 63, 33, 73, 43, 83, 53, 93 ];
+
+const sharp_sig_positions = [ 33, 63, 23, 53, 83, 43, 73 ];
+
+const SHARP_SVG_PATH = "M 5.0448 -16.5032 L 5.0448 -27.7928 L 9.8448 -29.1176 L 9.8448 -17.8856 L 5.0448 -16.5032 z M 14.496 -19.2344 L 11.196 -18.2888 L 11.196 -29.5208 L 14.496 -30.4424 L 14.496 -35.108 L 11.196 -34.1864 L 11.196 -45.6626 L 9.8448 -45.6626 L 9.8448 -33.8384 L 5.0448 -32.4584 L 5.0448 -43.6178 L 3.7704 -43.6178 L 3.7704 -32.0336 L 0.4704 -31.1096 L 0.4704 -26.4344 L 3.7704 -27.356 L 3.7704 -16.1456 L 0.4704 -15.2264 L 0.4704 -10.5704 L 3.7704 -11.492 L 3.7704 -0.0806 L 5.0448 -0.0806 L 5.0448 -11.9 L 9.8448 -13.22 L 9.8448 -2.1182 L 11.196 -2.1182 L 11.196 -13.6376 L 14.496 -14.5616 L 14.496 -19.2344 z";
 
 const FLAT_SVG_PATH = "M 10.6178 -15.1513 C 10.6178 -13.2883 9.9194 -11.504 8.0032 -9.1091 C 5.9731 -6.572 4.2642 -5.1199 2.0124 -3.4124 L 2.0124 -14.5325 C 2.5243 -15.8252 3.2792 -16.8718 4.2804 -17.6753 C 5.2783 -18.4756 6.2892 -18.8773 7.313 -18.8773 C 9.0043 -18.8773 10.0768 -17.9183 10.5401 -16.0067 C 10.5919 -15.8512 10.6178 -15.566 10.6178 -15.1513 z M 10.3748 -22.9273 C 8.9784 -22.9273 7.5593 -22.5418 6.1142 -21.7674 C 4.6692 -20.9963 3.3019 -19.9627 2.0124 -18.6764 L 2.0124 -42.2367 L 0.1883 -42.2367 L 0.1883 -1.8831 C 0.1883 -0.7426 0.4993 -0.1724 1.1214 -0.1724 C 1.481 -0.1724 1.9279 -0.4737 2.5956 -0.8722 C 4.4856 -2.0002 5.6637 -2.754 6.9437 -3.5493 C 8.4037 -4.4563 10.0476 -5.5156 12.2216 -7.5892 C 13.7218 -9.0958 14.8072 -10.6153 15.4811 -12.1446 C 16.1518 -13.6771 16.4887 -15.1934 16.4887 -16.7 C 16.4887 -18.9292 15.8958 -20.5135 14.71 -21.4499 C 13.3686 -22.4348 11.9203 -22.9273 10.3748 -22.9273 z";
