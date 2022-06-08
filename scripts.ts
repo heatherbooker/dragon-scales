60c3ac414d404684e5da41f7db9d118f556e1bac
dan soucy <dev@danso.ca>
2022-06-07 21:05:12 -0400

    remove blank lines with spaces

--- scripts.ts
+++ scripts.ts
@@ -142,12 +142,11 @@ window.addEventListener('load', function() {
     while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
     while(v === 0) v = Math.random();
     let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-    
+
     num = num / 10.0 + 0.5; // Translate to 0 -> 1
     if (num > 1 || num < 0) {
       num = getRandom(min, max); // resample between 0 and 1 if out of range
     }
-    
     else {
       num *= max - min; // Stretch to fill range
       num += min; // offset to min
