f8b037197b2dec0d318d59f14dfc9cd7e281b43e
dan soucy <dev@danso.ca>
2022-06-15 00:35:19 -0400

    separate type defs into src/types.ts

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,13 +1,3 @@
-type Note = {
-  letter: LetterName,
-  sharps: number, // sharps positive, natural zero, flats negative
-}
-
-type Scale = {
-  tonic: Note,
-  mode: ScaleType,
-}
-
 function render_note(note: Note): string {
   return note.letter + render_sharps(note.sharps);
 }
@@ -22,30 +12,6 @@ function render_sharps(sharps: number): string {
   }
 }
 
-
-const enum Interval {
-  PerfectUnison,
-  MinorSecond,
-  MajorSecond,
-  MinorThird,
-  MajorThird,
-  PerfectFourth,
-  PerfectFifth,
-  MinorSixth,
-  MajorSixth,
-  MinorSeventh,
-  MajorSeventh,
-};
-
-enum LetterName {
-  'A' = 'A',
-  'B' = 'B',
-  'C' = 'C',
-  'D' = 'D',
-  'E' = 'E',
-  'F' = 'F',
-  'G' = 'G',
-};
 
 function note_difficulty_weight(note: Note): number {
   // FIXME this is not terribly sophisticated.
@@ -70,8 +36,6 @@ function note_difficulty_weight(note: Note): number {
   }
   return difficulty;
 }
-
-type KeySig = { sharps: number, flats: number }
 
 function ionian_signatures_for_letter(letter: LetterName): number {
   switch (letter) {
@@ -99,52 +63,6 @@ function ionian_signatures(note: Note): KeySig {
       flats: 0,
     }
   }
-}
-
-enum ScaleType {
-  Ionian,
-  MelodicMinor,
-  HarmonicMinor,
-  DoubleHarmonic, // flat 2 and flat 6
-
-    // modes of ionian
-  Aeolian,
-  Dorian,
-  Phrygian,
-  Lydian,
-  Mixolydian,
-  Locrian,
-
-    // modes of melodic minor
-  MelodicMinorMode2,
-  MelodicMinorMode3,
-  Simpsons,
-  MelodicMinorMode5,
-  HalfDiminished,
-  AlteredDominant,
-
-    // modes of harmonic minor
-  HarmonicMinorMode2,
-  HarmonicMinorMode3,
-  UkrainianDorian,
-  PhrygianDominant,
-  HarmonicMinorMode6,
-  HarmonicMinorMode7,
-
-    // modes of double harmonic scale
-  DoubleHarmonicMode2,
-  DoubleHarmonicMode3,
-  HungarianMinor,
-  DoubleHarmonicMode5,
-  DoubleHarmonicMode6,
-  DoubleHarmonicMode7,
-
-    // others
-  Pentatonic,
-  WholeTone,
-  Chromatic,
-  OctatonicDominant,
-  OctatonicDiminished,
 }
 
 
@@ -192,18 +110,6 @@ const scale_types_difficulty: {[index in ScaleType]: number} = {
   [ScaleType.OctatonicDominant]: 0.5,
   [ScaleType.OctatonicDiminished]: 0.6,
 
-};
-
-const enum CheckBoxen {
-  "majors" = "majors",
-  "melodic-minor" = "melodic-minor",
-  "harmonic-minor" = "harmonic-minor",
-  "ionian-modes" = "ionian-modes",
-  "memimos" = "memimos", // modes of melodic minor
-  "pentatonic" = "pentatonic",
-  "whole-tone" = "whole-tone",
-  "chromatic" = "chromatic",
-  "octatonic" = "octatonic",
 };
 
 const scaletype_subsets: {[index in CheckBoxen]: ScaleType[]} = {
---
+++ src/types.ts
@@ -0,0 +1,100 @@
+type Note = {
+  letter: LetterName,
+  sharps: number, // sharps positive, natural zero, flats negative
+}
+
+
+type Scale = {
+  tonic: Note,
+  mode: ScaleType,
+}
+
+
+type KeySig = { sharps: number, flats: number }
+
+
+const enum Interval {
+  PerfectUnison,
+  MinorSecond,
+  MajorSecond,
+  MinorThird,
+  MajorThird,
+  PerfectFourth,
+  PerfectFifth,
+  MinorSixth,
+  MajorSixth,
+  MinorSeventh,
+  MajorSeventh,
+};
+
+
+enum LetterName {
+  'A' = 'A',
+  'B' = 'B',
+  'C' = 'C',
+  'D' = 'D',
+  'E' = 'E',
+  'F' = 'F',
+  'G' = 'G',
+};
+
+
+enum ScaleType {
+  Ionian,
+  MelodicMinor,
+  HarmonicMinor,
+  DoubleHarmonic, // flat 2 and flat 6
+
+    // modes of ionian
+  Aeolian,
+  Dorian,
+  Phrygian,
+  Lydian,
+  Mixolydian,
+  Locrian,
+
+    // modes of melodic minor
+  MelodicMinorMode2,
+  MelodicMinorMode3,
+  Simpsons,
+  MelodicMinorMode5,
+  HalfDiminished,
+  AlteredDominant,
+
+    // modes of harmonic minor
+  HarmonicMinorMode2,
+  HarmonicMinorMode3,
+  UkrainianDorian,
+  PhrygianDominant,
+  HarmonicMinorMode6,
+  HarmonicMinorMode7,
+
+    // modes of double harmonic scale
+  DoubleHarmonicMode2,
+  DoubleHarmonicMode3,
+  HungarianMinor,
+  DoubleHarmonicMode5,
+  DoubleHarmonicMode6,
+  DoubleHarmonicMode7,
+
+    // others
+  Pentatonic,
+  WholeTone,
+  Chromatic,
+  OctatonicDominant,
+  OctatonicDiminished,
+}
+
+
+const enum CheckBoxen {
+  "majors" = "majors",
+  "melodic-minor" = "melodic-minor",
+  "harmonic-minor" = "harmonic-minor",
+  "ionian-modes" = "ionian-modes",
+  "memimos" = "memimos", // modes of melodic minor
+  "pentatonic" = "pentatonic",
+  "whole-tone" = "whole-tone",
+  "chromatic" = "chromatic",
+  "octatonic" = "octatonic",
+};
+
